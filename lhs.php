93471f3dccd8f99d9415823ff13f2cd5b476c143
Sandeep Pal <sandeepbii@yahoo.com>
2022-04-29 12:43:35 +0000

    Removed bug from previous implementatation about Chembl assay id and Chembl assay type

--- lhs.php
+++ lhs.php
@@ -308,7 +308,7 @@
                      $master_arr_chembl[] = array($rowsa[$i]['id_a'], $rowsb[$i]['id_b'],
                                                   $rowsa[$i]['smiles_a'], $rowsb[$i]['smiles_b'],
                                                   $rowsa[$i]['chmbl_clearance'], $rowsb[$i]['chmbl_clearance'],
-                                                  $rowsb[$i]['assay_chmbl_id'],$rowsb[$i]['assay_type'],
+                                                  $rowsa[$i]['assay_chmbl_id'],$rowsa[$i]['assay_type'],
                                                   $rowsa[$i]['chmbl_herg'],$rowsb[$i]['chmbl_herg'],
                                                   $rowsa[$i]['chmbl_F_percent'],$rowsb[$i]['chmbl_F_percent'],
                                                   $rowsa[$i]['chmbl_half_life'],$rowsb[$i]['chmbl_half_life'],
