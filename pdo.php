dba38b5e306385fbb51fc1634cdf12ad7eeeafa9
Sandeep Pal <sandeepbii@yahoo.com>
2022-08-12 11:04:48 +0100

    Checking commit

--- pdo.php
+++ pdo.php
@@ -4,7 +4,7 @@ try{
 
   // $myPDO = new PDO("pgsql:host=localhost, dbname=postgres", "postgres", "sql");
   $pdo = new PDO('mysql:host=localhost;port=3306;dbname=mmpdb',
-     'sandeep', 'sql');
+     'sandeep', '$chrodinger123');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   // echo("Connected");
